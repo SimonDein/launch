e14a41d11ac5247f54ab33020d9a1126c4e99f1a
Simon Dein <simon.dein@gmail.com>
2019-03-11 21:08:41 +0100

    Finished html and css course. Initiated JS basics

---
+++ course_js210_computational_thinking_and_javascript_programming/lesson_1_javascript_basics/book_introduction_to_javascript_programming/javascript_style_and_conventions.js
@@ -0,0 +1,25 @@
+// Single line comments
+
+/* Multiline
+   comments */
+
+// Use 2 spaces for indentation
+
+// Use camelCase for variable and function names
+// initializing a variable
+let answerToUltimateQuestion = 42;
+
+// defining a function
+function fourScoreAndSevenYearsAgo() {
+  // do something
+}
+
+// defining a constructor
+function DomesticCat(name) {
+  // do something
+}
+
+// Use uppercase names with underscores to represent const values: values that don't change
+const INTEREST_RATE = 0.0525;
+const FOUR = 'four';
+
