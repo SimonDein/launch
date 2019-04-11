151e5ec4c792aca10a11fd665f9747938b2b172a
Simon Dein <simon.dein@gmail.com>
2019-04-11 17:35:13 +0200

    fixed extra output of empty line

--- course_js210_computational_thinking_and_javascript_programming/lesson_3_practice_problems_logic_and_flow_control/splitString.js
+++ course_js210_computational_thinking_and_javascript_programming/lesson_3_practice_problems_logic_and_flow_control/splitString.js
@@ -5,8 +5,24 @@
 
 // You may use the square brackets ([]) to access a character by index (as shown below), and the length property to find the string length. However, you may not use any other properties or methods from JavaScript's built-in String class.
 
-function splitString(string, delimiter='') {
+function splitString(string, delimiter = null) {
+  if (delimiter === null) {
+    console.log("ERROR: no delimter");
+  }
   
+  let splitString = '';
+  
+  for (i = 0; i< string.length; i++) {
+    if (string[i] === delimiter) {
+      console.log(splitString);
+      splitString = '';
+    } else if(delimiter === '') {
+      console.log(string[i])
+    } else {
+      splitString += string[i];
+    }
+  }
+  if (splitString) console.log(splitString);
 }
 
 // The function logs the split strings to the console, as shown below:
