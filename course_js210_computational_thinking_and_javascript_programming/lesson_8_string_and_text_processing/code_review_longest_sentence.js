6ca353fb194b3328e3158bc05ebc7185425166fd
Simon Dein <simon.dein@gmail.com>
2019-07-08 13:16:48 +0200

    Sentence delimeters are kept instead of removed.

--- course_js210_computational_thinking_and_javascript_programming/lesson_8_string_and_text_processing/code_review_longest_sentence.js
+++ course_js210_computational_thinking_and_javascript_programming/lesson_8_string_and_text_processing/code_review_longest_sentence.js
@@ -31,7 +31,7 @@ var longText = 'Four score and seven years ago our fathers brought forth' +
   ' earth.';
 
 function mostWordsSentence(text) {
-  let sentences = text.split(/\w[.!?] ?/);
+  sentences = text.match(/\w.*?[.!?]/mg);
   let wordcount;
   let sentenceIndex;
 
