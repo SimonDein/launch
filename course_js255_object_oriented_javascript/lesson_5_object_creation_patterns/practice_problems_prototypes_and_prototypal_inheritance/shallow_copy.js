88b9efd39aaee6377996bd2adc00489e2462a192
Simon Dein <simon.dein@gmail.com>
2019-11-04 22:46:33 +0100

    exercise

---
+++ course_js255_object_oriented_javascript/lesson_5_object_creation_patterns/practice_problems_prototypes_and_prototypal_inheritance/shallow_copy.js
@@ -0,0 +1,26 @@
+// Write a function to provide a shallow copy of an object.
+// The object that you copy should share the same prototype chain as the original object, and it should have the same own properties that return the same values or objects when accessed.
+// Use the code below to verify your implementation:
+
+function shallowCopy(object) {
+  let shallowCopy = Object.create(Object.getPrototypeOf(object));
+  let ownProps = Object.getOwnPropertyNames(object).forEach((key) => shallowCopy[key] = object[key]);
+  return shallowCopy;
+}
+
+var foo = {
+  a: 1,
+  b: 2,
+};
+
+var bar = Object.create(foo);
+bar.c = 3;
+bar.say = function() {
+  console.log('c is ' + this.c);
+};
+
+var baz = shallowCopy(bar);
+console.log(baz.a);       // => 1
+baz.say();                // => c is 3
+console.log(baz.hasOwnProperty('a'));  // false
+console.log(baz.hasOwnProperty('b'));  // false
