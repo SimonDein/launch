9cda48a38d72d911721078dcc42cd447d4b015bc
Simon Dein <simon.dein@gmail.com>
2019-11-20 00:26:39 +0100

    first, last, without, range and lastIndexOf tested and added to library

--- course_js255_object_oriented_javascript/assessment/practice_project/my_utility_library.js
+++ course_js255_object_oriented_javascript/assessment/practice_project/my_utility_library.js
@@ -11,15 +11,49 @@ let _ = function(obj) {
       return obj[obj.length - 1];
     },
 
-// without, return a new array that does not contain the value passed to it.
-    without: function(value) {
-
+    // without, return a new array that does not contain the value passed to it.
+    without: function(match) {
+      return obj.reduce((arr, val) => {
+        if (match === val || Number.isNaN(match) && Number.isNaN(val)) {
+          return arr;
+        }
+        arr.push(val);
+        return arr;
+      }, []);
     },
 // range, return an array of values starting at 0 when a single number is supplied.
 // If two arguments are supplied, returns an array of values that start with the first argument and ends with the one number less than the second argument.
+    range: function(...indexes) {
+      let startIndex = 0;
+      let endIndex
 
+      if (indexes.length >= 2) {
+        startIndex = indexes[0] > 0 ? indexes[0] : 0;
+        endIndex = indexes[1] > 0 ? indexes[1] : 0;
+      } else {
+        endIndex = indexes[0];
+      }
+
+      endIndex = endIndex > obj.length - 1 ? obj.length : endIndex;
+
+      let a = [];
+
+      for(let i = startIndex; i < endIndex; i += 1) {
+        a.push(obj[i]);
+      }
+
+      return a;
+    },
 // lastIndexOf, return the last index of the supplied value.
-
+    lastIndexOf: function(value) {
+      let i = obj.length - 1;
+      for(; i >= 0; i -= 1) {
+        if (obj[i] === value || Number.isNaN(value) && Number.isNaN(value)) {
+          return i;
+        };
+      }
+      return -1;
+    },
 // sample, return a single value from an array when no argument is supplied. Return an array of multiple, non-repeated elements when a quantity is supplied.
   };
 }
--- course_js255_object_oriented_javascript/assessment/practice_project/my_utility_library.test.js
+++ course_js255_object_oriented_javascript/assessment/practice_project/my_utility_library.test.js
@@ -39,6 +39,58 @@ describe('_([]).without', () => {
   });
 
   test('passed number, returns array without the number', () => {
-    expect(_([1, 3, 4, 3, 4, 6, 2, 3]).without(3)).not.arrayContaing([3]);
+    expect(_([1, 3, 4, 3, 4, 6, 2, 3]).without(3)).toEqual(expect.not.arrayContaining([3]));
+  });
+
+  test('passed string, returns array without the string', () => {
+    expect(_(['yo', 'hello', 'yo', 'hi', 'y']).without('yo')).toEqual(expect.not.arrayContaining(['yo']));
+  });
+
+  test('passed NaN , returns array without NaN', () => {
+    expect(_([1, 2, NaN, 34]).without(NaN)).toEqual(expect.not.arrayContaining([NaN]));
+  });
+
+  test('returns an array with the length shortened by the number of elements removed', () => {
+    expect(_([1, 3, 4, 3, 4, 6, 2, 3]).without(3).length).toBe(5);
+  });
+});
+
+describe('_([]).range', () => {
+  test('is defined', () => {
+      expect(_).toEqual(expect.anything());
+  });
+
+  test('will return values from index 0 up to passed index, when passed single argument', () => {
+    expect(_([0, 1, 2, 3, 4]).range(3)).toEqual([0, 1, 2]);
+  });
+
+  test('will return values from start index up end index, when passed two arguments', () => {
+    expect(_([0, 1, 2, 3, 4]).range(2, 5)).toEqual([2, 3, 4]);
+  });
+
+  test('will start at index 0 given negative start index', () => {
+    expect(_([1, 2, 3, 4]).range(-1, 2)).toEqual([1, 2]);
+  });
+
+  test('will end at end of index given an index greater than arrays length', () => {
+    expect(_([1, 2, 3, 4]).range(10)).toEqual([1, 2, 3, 4]);
+  });
+});
+
+describe('_([]).lastIndexOf', () => {
+  test('returns last index of passed string', () => {
+    expect(_(['hello', 'yo', 'hello', 'hi']).lastIndexOf('hello')).toBe(2);
+  });
+
+  test('returns last index of passed number', () => {
+    expect(_([1, 2, 3, 3, 2, 5]).lastIndexOf(2)).toBe(4);
+  });
+
+  test('returns last index of NaN', () => {
+    expect(_([NaN, 2, NaN, 'NaN']).lastIndexOf(NaN)).toBe(3);
+  });
+
+  test('returns -1 when no match', () => {
+    expect(_([11, 4, 3, 'hello', true, false, NaN, undefined, null]).lastIndexOf(1)).toBe(-1);
   });
 });
