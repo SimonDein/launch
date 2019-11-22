01b3279aa93fb960700621807fc7b9d1fbe0ef49
Simon Dein <simon.dein@gmail.com>
2019-11-22 14:16:40 +0100

    Todolist, todoManager and makeTodo API finished

--- course_js255_object_oriented_javascript/assessment/take_home_project/todoApp.js
+++ course_js255_object_oriented_javascript/assessment/take_home_project/todoApp.js
@@ -1,45 +1,73 @@
+// todoManager wrapper function
 function todoManager(todoList) {
   return {
-    
+    getTodos: function() {
+      return todoList.getTodos();
+    },
+
+    getCompletedTodos: function() {
+      return this.getTodos().filter((todo) => todo.completed === true);
+    },
+
+    getTodosWithin(month, year) {
+      return this.getTodos().filter((todo) => todo.month === month && todo.year === year);
+    },
+
+    getCompletedTodosWithin(month, year) {
+      return this.getTodos().filter((todo) => {
+        todo.month === month &&
+        todo.year === year &&
+        todo.completed === true;
+      })
+    }
   }
 }
 
+
 // TodoList object (OLOO)
-let TodoList = (function() {
-  let todos = []; // TODO: - does not work for private data
+function TodoList(todoDataSet) {
+  let todos = [];
 
-  return {
-    init: function(todoDataSet) {
-      if (todoDataSet !== undefined) { // Initialize with N todos if passed
-        todoDataSet.forEach((todoData) => todos.push(makeTodo(todoDataSet)));
-      };
-    },
+  if (todoDataSet !== undefined) {
+    todoDataSet.forEach((todoData) => todos.push(makeTodo(todoDataSet)));
+  };
 
-    getTodos: function() {
-      return todos.splice();
-    },
+  this.getTodos = function() {
+    return todos.slice();
+  };
 
-    addTodo: function(todoData) {
-      todos.push(makeTodo(todoData));
-    },
+  this.addTodo = function(todoData) {
+    todos.push(makeTodo(todoData));
+  };
 
-    deleteTodo: function(todo) {
-      let index = todos.indexOf(todo);
-      todos.splice(index, 1);
-    },
+  this.deleteTodo = function(todo) {
+    let index = todos.findIndex((t) => t.id === Number(todo.id));
+    let deletedTodo = todos[index];
+    todos.splice(index, 1);
 
-    updateTodo: function(updatedTodo) {
-      let id = updatedTodo.id;
-      let outdatedTodo = this.getTodoById(id);
-      Object.defineProperties(outdatedTodo, updatedTodo);
-    },
+    return deletedTodo;
+  };
 
-    getTodoById: function(id) {
-      let index = todos.findIndex((todo) => todo.id === id)
-      return todos[index];
-    },
-  }
-})();
+  this.updateTodo = function(updatedTodo) {
+    let id = updatedTodo.id;
+    let outdatedTodo = this.getTodoById(id);
+
+    Object.keys(updatedTodo).forEach((key) => {
+      Object.defineProperty(outdatedTodo, key, {
+        value: updatedTodo[key],
+        writable: false,
+        configurable: true,
+      })
+    });
+    //Object.defineProperties(outdatedTodo, updatedTodo);
+  };
+
+  this.getTodoById = function(id) {
+    let index = todos.findIndex((todo) => todo.id === id)
+    return todos[index];
+  };
+}
+
 
 // makeTodo 'object factory' (based on OLOO)
 let makeTodo = (function() {
@@ -62,26 +90,32 @@ let makeTodo = (function() {
       title: {
         value:  todoData.title || '',
         writable: false,
+        configurable: true,
       },
       month: {
         value:  todoData.month || '',
         writable: false,
+        configurable: true,
       },
       year: {
         value:  todoData.year || '',
         writable: false,
+        configurable: true,
       },
       description: {
         value:  todoData.description || '',
         writable: false,
+        configurable: true,
       },
       completed: {
         value:  false,
         writable: false,
+        configurable: true,
       },
       id: {
         value:  getNextId(),
         writable: false,
+        configurable: true,
       },
     });
 
@@ -89,9 +123,8 @@ let makeTodo = (function() {
   };
 })();
 
-
-
 module.exports = {
   makeTodo: makeTodo,
   TodoList: TodoList,
+  todoManager: todoManager,
 };
