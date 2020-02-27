b461ffbace3da463e97476d12a6865ad3fd6801c
Simon Dein <simon.dein@gmail.com>
2020-02-27 17:15:45 +0100

    exercise text added

--- course_js230_DOM_and_asynchronous_programming/exercises/3_tracing_the_dom_tree/script.js
+++ course_js230_DOM_and_asynchronous_programming/exercises/3_tracing_the_dom_tree/script.js
@@ -1,3 +1,7 @@
+/*
+Write a JavaScript function that takes an element's id and returns the DOM tree of the element in a two-dimensional array. The first subarray contains the element and its siblings, the second contains the parent of the element and its siblings, so on and so forth, all the way up to the "grandest" parent. Assume that the grandest parent is the element with an id of "1".
+*/
+
 function domTreeTracer(id) {
   let domStack = [];
   let currentElement = document.getElementById(id);
